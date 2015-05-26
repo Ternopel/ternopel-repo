cfe072555dc6dd834a15d0e7994ea8f339fbfcb6
Maximiliano Carrizo <mcarrizo@zintro.com>
2015-05-26 12:52:57 -0300

    First commit !

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express	= require('express');
+var router	= express.Router();
+var dots;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	/* res.render('index', { title: 'Express' }); */
+	console.log("Luisito");
+	if(dots) {
+		console.log("Tiene valor");
+	}
+	return res.status(200).send(dots.index());
 });
 
-module.exports = router;
+
+
+module.exports = function(dots2) {
+	dots = dots2;
+
+	if(dots) {
+		console.log("XXTiene valor");
+	}
+
+	
+	return router;
+}
