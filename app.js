5c3541edf8048c6e3f105c2b8657fce3578b335b
mcarrizo <mcarrizo@zintro.com>
2015-05-27 05:42:09 -0300

    Remove users

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ logger.info("configuring /");
 var routes = require('./routes/index')(dots);
 app.use('/', routes);
 
-logger.info("configuring /users");
-var users = require('./routes/users');
-app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
