1b6a1baa8c379a9908c84b48de89922515a05758
mcarrizo <mcarrizo@zintro.com>
2015-05-27 05:20:21 -0300

    Express config !

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var dots = require("dot").process({path: "./views"});
-
-console.log("ACA");
-console.log(dots);
-
-var routes = require('./routes/index')(dots);
-var users = require('./routes/users');
+var logger			= require("./utils/logger");
+var express			= require('express');
+var dots			= require("dot").process({path: "./views"});
+var expressConf		= require("./utils/express-config");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html'); 
+logger.info("configuring express....");
+expressConf.init(app, express);
+logger.info("Express configured");
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+logger.info("configuring /");
+var routes = require('./routes/index')(dots);
 app.use('/', routes);
+
+logger.info("configuring /users");
+var users = require('./routes/users');
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -41,24 +29,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,10 @@ var dots;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	/* res.render('index', { title: 'Express' }); */
-	console.log("Luisito");
-	if(dots) {
-		console.log("Tiene valor");
-	}
 	return res.status(200).send(dots.index());
 });
 
-
-
-module.exports = function(dots2) {
-	dots = dots2;
-
-	if(dots) {
-		console.log("XXTiene valor");
-	}
-
-	
+module.exports = function(dotsparam) {
+	dots = dotsparam;
 	return router;
 }
