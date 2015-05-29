8f2513cb024543867260e7f22d66d3ac59debe7e
mcarrizo <mcarrizo@zintro.com>
2015-05-28 22:14:34 -0300

    All

--- app.js
+++ app.js
@@ -3,45 +3,17 @@ var logger			= require("./utils/logger"),
 	dots			= require("dot").process({path: "./views"}),
 	expressConf		= require("./utils/express-config");
 
-var app = express();
+var app = express(); 
 
 logger.info("configuring express....");
-expressConf.init(app, express);
+expressConf.init(app, express); 
 logger.info("Express configured");
 
 logger.info("configuring /");
-var routes = require('./routes/index')(dots);
+var routes = require('./routes/index')(dots); 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
+logger.info("Adding error routes");
+expressConf.addErrorRoutes(app);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-var express	= require('express');
-var router	= express.Router();
+var express	= require('express'),
+	router	= express.Router(),
+	logger	= require("../utils/logger");
+
 var dots;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	return res.status(200).send(dots.index());
+	var index = dots.index();
+	logger.info(JSON.stringify(index));
+	return res.status(200).send(index);
 });
 
 module.exports = function(dotsparam) {
