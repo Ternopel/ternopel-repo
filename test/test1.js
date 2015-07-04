4fc0ac347dae22199b80ca1554325a8422eb9662
Maximiliano <mcarrizo@ternopel.com>
2015-07-04 13:52:55 -0300

    Fix remove selenium

--- test/test1.js
+++ test/test1.js
@@ -3,11 +3,10 @@ var app			= require(__dirname+'/../app.js'),
 	config		= require(__dirname+'/../utils/config')();
 
 var request		= require('supertest'),
-	soda		= require('soda'),
-	assert		= require('assert'),
-	expect		= require('expect'),
-	browser,
-	server;
+//	assert		= require('assert'),
+//	expect		= require('expect'),
+	server,
+	db;
 
 describe('Users creation', function() {
 	
@@ -17,102 +16,45 @@ describe('Users creation', function() {
 		
 		config.app_run_liquibase	= 'false';
 		config.db_database			= config.test_db_datatabase;
+		config.db_show_sql			= config.test_db_show_sql;
 		
-		
-		var waterfall = require('async-waterfall');
-		waterfall([ 
-			function(callback) {
-				logger.info("Initiating app");
-				app.init(logger,config, function(app,db) {
-					return callback(null,app,db);
-				});
-			}, 
-			function(app,db,callback) {
-				logger.info("Starting TEST listener");
-				server = app.listen(config.test_app_port, function() {
-					logger.info('Listening on port:'+server.address().port);
-					return callback(null,db);
-				});
-			}, 
-			function(db,callback) {
-				logger.info("Dropping model");
-				db.drop(function(err) {
-					return callback(err,db);
-				});
-			}, 
-			function(db,callback) {
-				logger.info("Creating model");
-				db.sync(function(err) {
-					return callback(err);
-				});
-			},
-			function(callback) {
-				logger.info("Starting browser");
-				browser = soda.createClient({
-					host: 'localhost',
-					port: 4444,
-					url: 'http://localhost:'+config.test_app_port,
-					browser: 'googlechrome'
-				});
-				return callback(null);
-			}
-		], 
-		function(err, result) {
-			done(err);
+		logger.info("Initiating app");
+		app.init(logger,config, function(app,pdb) {
+			db		= pdb;
+			server	= app.listen(config.test_app_port, function() {
+				logger.info('Listening on port:'+server.address().port);
+				return done();
+			});
 		});
 	});
+	
+	beforeEach(function(done) {
+		logger.info("Dropping model");
+		db.drop(function(err) {
+			if(err) {
+				return done(err);
+			}
+			logger.info("Creating model");
+			db.sync(function(err) {
+				return done(err);
+			});
+		});
+	});	
+	
 
  	it('Create not existing user', function(done) {
- 		console.log("EN TEST1");
- 		
- 		browser
-			.chain
-			.session()
-			.open('/')
-			.clickAndWait('register')
-			.type('email_address', 'natalita@gmail.com')
-			.type('password', 'natalita')
-			.clickAndWait('submit')
-			.assertTextPresent('Bienvenido, natalita@gmail.com')
-			.clickAndWait('logout')
-			.end(function(err){
-				browser.testComplete(
-					function() {
-						console.log('done');
-						return done(err);
-					});
-			}); 		
- 		
+ 		logger.info("EN TEST1");
+ 		return done();
 	});
 
  	it('Sign up with noexisting email', function(done) {
 
- 		console.log("EN TEST2");
- 		browser
-		.chain
-		.session()
-		.open('/')
-		.clickAndWait('register')
-		.type('email_address', 'natalita3@gmail.com')
-		.type('password', 'natalita')
-		.clickAndWait('submit')
-		.assertTextPresent('Bienvenido, natalita3@gmail.com')
-		.clickAndWait('logout')
-		.end(function(err){
-			browser.testComplete(
-				function() {
-					console.log('done');
-					return done(err);
-				});
-		}); 		
- 		
- 		
+ 		logger.info("EN TEST2");
  		return done();
  	});
  	
 	after(function (){
-		console.log('Stopping');
-		server.stop();
-		browser.close();
+		logger.info('Stopping');
+		server.close();
 	});
 });
