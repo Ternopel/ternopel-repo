03276f53a3a6d763a9b0c53d56ad6efb40ea9e04
Maximiliano <mcarrizo@ternopel.com>
2015-08-23 07:37:26 -0300

    Improvements !

--- public/javascript/products.js
+++ public/javascript/products.js
@@ -21,57 +21,90 @@ $(function () {
 		if(check.is(':checked')) {
 			formatstr.show();
 			
-				$('.upload-photo-container').attr('style', 'background: url(/admin/productspictures/'+trid+'); ');
-		
-				var options = {
-					imageBox: '.imageBox',
-					thumbBox: '.thumbBox',
-					spinner: '.spinner',
-					imgSrc: 'avatar.jpg'
-				};
-		
-				var cropper;
-				document.querySelector('#picture').addEventListener('change', function(){ 
-					console.log('Firing picture change');
-					file_name = $('#picture').val();
-					var reader = new FileReader();
-					reader.onload = function(e) {
-						console.log('Firing reader on load');
-						var image = new Image();
-						image.src = e.target.result;
-						image.onload = function(evt) {
-							console.log('Firing image on load');
-							if (this.width > 450 && this.height > 300) {
-								options.imgSrc = e.target.result;
-								cropper = new cropbox(options);
-								$('.upload-btn').attr('class', 'change-btn');
-								$('.upload-label').html('Cambiar foto');
+			$("div[name='"+trid+"-upload-photo-container']").attr('style', 'background: url(/admin/productspictures/'+trid+'); ');
+	
+			var options = {
+				imageBox: "div[name='"+trid+"-imageBox']",
+				thumbBox: "div[name='"+trid+"-thumbBox']",
+				spinner: "div[name='"+trid+"-spinner']",
+				imgSrc: 'avatar.jpg'
+			};
+	
+			var cropper;
+			document.querySelector("input[name='"+trid+"-picture']").addEventListener('change', function(){ 
+				console.log('Firing picture change');
+				file_name = $("input[name='"+trid+"-picture']").val();
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					console.log('Firing reader on load');
+					var image = new Image();
+					image.src = e.target.result;
+					image.onload = function(evt) {
+						console.log('Firing image on load');
+						if (this.width > 450 && this.height > 300) {
+							options.imgSrc = e.target.result;
+							cropper = new cropbox(options);
+							$("span[name='"+trid+"-upload-btn']").attr('class', 'change-btn');
+							$("span[name='"+trid+"-upload-label']").html('Cambiar foto');
 
-								$('#btnZoomIn').show();
-								$('#btnZoomOut').show();
-								$('#savepicture').show();
-							} 
-							else {
-								alert('Imagen debe ser mayor a 452x x 300px');
+							$("input[name='"+trid+"-btnZoomIn']").show();
+							$("input[name='"+trid+"-btnZoomOut']").show();
+							$("button[name='"+trid+"-save']").show();
+						} 
+						else {
+							alert('Imagen debe ser mayor a 452x x 300px');
 //								show_error_messages({responseText:[{param:'general',msg:'Imagen debe ser mayor a 452x x 300px'}]});
-							}
 						}
-					};
-					reader.readAsDataURL(this.files[0]);
+					}
+				};
+				reader.readAsDataURL(this.files[0]);
+			});
+	
+			document.querySelector("input[name='"+trid+"-btnCrop']").addEventListener('click', function() {
+				console.log('Firing crop button');
+				var img = cropper.getDataURL();
+				document.querySelector("div[name='"+trid+"-cropped']").innerHTML += '<img src="'+img+'">';
+			});
+			document.querySelector("input[name='"+trid+"-btnZoomIn']").addEventListener('click', function() {
+				console.log('Zoom in');
+				cropper.zoomIn();
+			});
+			document.querySelector("input[name='"+trid+"-btnZoomOut']").addEventListener('click', function() {
+				console.log('Zoom out');
+				cropper.zoomOut();
+			});
+			
+			$("button[name='"+trid+"-save']").click(function () {
+				console.log('Saving picture');
+				var csrf		=$("input[name='_csrf']").val();
+				
+				$("input[name='"+trid+"-btnCrop']").click().promise().done(function () {
+					console.log('After crop button firing');
+					
+					var img_data	= $("div[name='"+trid+"-cropped'] img").attr('src');
+					if (img_data) {
+						console.log('Picture has data to send');
+						var picture_regex	= /data:(.*);base64,(.*)/;
+						var type			= img_data.replace(picture_regex, '$1');
+						var data			= img_data.replace(picture_regex, '$2');
+						
+						var formdata	={_csrf:csrf, product_id:trid, type:type, data:data};
+						$.ajax({
+							url : '/admin/productspictures',
+							type : 'POST',
+							data : formdata,
+							success : function (entity) {
+								console.log(entity);
+								$("#showpicture_"+trid).click();
+								$('tr[name='+trid+'_picture]').hide();
+							},
+							error : function(errorresponse) {
+								show_error_messages(errorresponse);
+							}
+						});
+					}
 				});
-		
-				document.querySelector('#btnCrop').addEventListener('click', function() {
-					var img = cropper.getDataURL();
-					document.querySelector('.cropped').innerHTML += '<img src="'+img+'">';
-				});
-				document.querySelector('#btnZoomIn').addEventListener('click', function() {
-					console.log('Zoom in');
-					cropper.zoomIn();
-				});
-				document.querySelector('#btnZoomOut').addEventListener('click', function() {
-					console.log('Zoom out');
-					cropper.zoomOut();
-				});
+			});
 		}
 		else {
 			formatstr.hide();
@@ -83,40 +116,6 @@ $(function () {
 $(function () {
 	$("#addProduct").click(function () {
 		add('/admin/products');
-	});
-});
-
-// Add product
-$(function () {
-	$("#savepicture").click(function () {
-		console.log('Saving picture');
-		var trid		=$(this).attr('name');
-		var csrf		=$("input[name='_csrf']").val();
-		
-		$("#btnCrop").click().promise().done(function () {
-			var img_data	= $('.cropped img').attr('src');
-			if (img_data) {
-				console.log('Picture has data to send');
-				var picture_regex	= /data:(.*);base64,(.*)/;
-				var type			= img_data.replace(picture_regex, '$1');
-				var data			= img_data.replace(picture_regex, '$2');
-				
-				var formdata	={_csrf:csrf, product_id:trid, type:type, data:data};
-				$.ajax({
-					url : '/admin/productspictures',
-					type : 'POST',
-					data : formdata,
-					success : function (entity) {
-						console.log(entity);
-						$("#showpicture_"+trid).click();
-						$('tr[name='+trid+'_picture]').hide();
-					},
-					error : function(errorresponse) {
-						show_error_messages(errorresponse);
-					}
-				});
-			}
-		});
 	});
 });
 
