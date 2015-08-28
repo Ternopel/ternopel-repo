19b42e60344def97435d3cba2b532059175d0c9a
Maximiliano <mcarrizo@ternopel.com>
2015-08-28 20:13:49 -0300

    .

---
+++ public/javascript/offers.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".slider-container").jcarousel({
+    animation: {
+        duration: 800,
+        easing:   'linear',
+        complete: function() {
+        }
+    }
+});
+});
