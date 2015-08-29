f1da2269350efbf2e332e80001ecf5e5f6f094b2
Maximiliano <mcarrizo@ternopel.com>
2015-08-29 16:00:11 -0300

    Carousel is working !

--- public/javascript/offers.js
+++ public/javascript/offers.js
@@ -1,10 +1,14 @@
 $(function() {
-	$(".slider-container").jcarousel({
-    animation: {
-        duration: 800,
-        easing:   'linear',
-        complete: function() {
-        }
-    }
-});
+	$('.slider-container ul').anoSlide(
+	{
+		items: 1,
+		speed: 500,
+		lazy: true,
+		auto: 4000,
+		onStart: function(ui) {
+			var index = ui.index;
+			$('.slider-btns li a').removeClass();
+			$('.slider-btns li:eq( '+index+' ) a').addClass('active-slide');
+		}
+	});
 });
