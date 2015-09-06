df9b45fb9efb3036653e261e6ddc6aae336ecf0a
Maximiliano <mcarrizo@ternopel.com>
2015-09-05 22:56:27 -0300

    Release.sh fix !

--- support/liquibase/release.sh
+++ support/liquibase/release.sh
@@ -1,4 +1,5 @@
-java -cp postgresql-9.3-1101-jdbc4.jar -jar liquibase-core-2.0.3.jar \
+export CLASSPATH="postgresql-9.3-1101-jdbc4.jar;liquibase-core-2.0.3.jar"
+java liquibase.integration.commandline.Main \
       --driver=org.postgresql.Driver \
       --changeLogFile=liquibase.xml \
       --url=jdbc:postgresql://localhost:5432/ternopel?charSet=UTF-8 \
