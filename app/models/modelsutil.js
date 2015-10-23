c37d6063b77627501d7129cb7b1edc7b1080a820
Maximiliano <mcarrizo@ternopel.com>
2015-10-23 20:17:50 -0300

    .

--- app/models/modelsutil.js
+++ app/models/modelsutil.js
@@ -1,17 +1,27 @@
 'use strict';
 
+
 function fillProductFormat(product,productformat) {
 	var retaildescription		= '';
 	var wholesaledescription	= '';
-	if(productformat.quantity===1) {
-		retaildescription += product.packaging.name;
+	var begin					= '<span class="price-con">$';
+	var end						= '</span>';
+	
+	if( ( productformat.units%1 ) !=0) {
+		// Ejemplo de esta condicion: Bobinas de papel diario
+		retaildescription += productformat.units.toFixed(2)+' '+product.packaging.name + 's de ' + productformat.format+' a '+begin+productformat.retail.toFixed(2)+end+' c/u';
+	}
+	else if ( productformat.units === 1 ) {
+		// Ejemplo de esta condicion: Bandas elasticas
+		retaildescription += productformat.units.toFixed(2) +' '+product.packaging.name + ' de ' + productformat.format+' a '+begin+productformat.retail.toFixed(2)+end;
 	}
 	else {
-		retaildescription += productformat.quantity + ' ' + product.packaging.name + 's';
+		// Ejemplo de esta condicion: Blondas de papel caladas
+		retaildescription += product.packaging.name +' '+ productformat.units + ' unid. de ' + productformat.format+' a '+begin+productformat.retail.toFixed(2)+end+' c/u';
 	}
-	retaildescription		+= ' de ' + productformat.format + ' a ';
-	wholesaledescription	+= productformat.units + ' ' + product.packaging.name + 's a ';
 	
+	wholesaledescription	+= productformat.quantity+' '+product.packaging.name+'s de '+begin+productformat.wholesale.toFixed(2)+end+' c/u a '+begin+(productformat.wholesale*productformat.quantity).toFixed(2)+end;
+		
 	productformat.retaildescription		= retaildescription;
 	productformat.wholesaledescription	= wholesaledescription;
 }
