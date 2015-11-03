e7269d86cd5550e704b2bf296a1cf8ab2ff28999
Maximiliano <mcarrizo@ternopel.com>
2015-11-03 10:27:01 -0300

    .

--- public/javascript/categories.js
+++ public/javascript/categories.js
@@ -11,9 +11,9 @@ $(function () {
 $(function () {
 	$("ul#ulproducts img").each(function(){
 		$(this).hover(function() {
-			$(this).attr('style','transform: scale(1.1) rotate(3deg);');
+			$(this).attr('style','transform: scale(1.1) rotate(3deg); transition: all 0.3s ease 0s;');
 		}, function() {
-			$(this).attr('style','');
+			$(this).attr('style','transition: all 0.3s ease 0s;');
 		});		
 	});
 });
