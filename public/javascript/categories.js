f5c6bc982f20d929e187155e06966e74f43af997
Maximiliano <mcarrizo@ternopel.com>
2015-11-03 10:23:09 -0300

    .

--- public/javascript/categories.js
+++ public/javascript/categories.js
@@ -8,10 +8,12 @@ $(function () {
 });
 
 // Add hover efect to all images
-/*
 $(function () {
 	$("ul#ulproducts img").each(function(){
-		$(this).hoverpulse();
+		$(this).hover(function() {
+			$(this).attr('style','transform: scale(1.1) rotate(3deg);');
+		}, function() {
+			$(this).attr('style','');
+		});		
 	});
 });
-*/
