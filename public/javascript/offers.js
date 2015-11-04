25049bbf8f806ee2d42c487a685231c5401408d4
Maximiliano Carrizo <mcarrizo@ternopel.com>
2015-11-04 10:53:54 -0300

    .

--- public/javascript/offers.js
+++ public/javascript/offers.js
@@ -1,14 +1,16 @@
 $(function() {
-	$('.slider-container ul').anoSlide(
-	{
-		items: 1,
-		speed: 500,
-		lazy: true,
-		auto: 4000,
-		onStart: function(ui) {
-			var index = ui.index;
-			$('.slider-btns li a').removeClass();
-			$('.slider-btns li:eq( '+index+' ) a').addClass('active-slide');
-		}
-	});
+	console.log($('.slider-container ul').children().size());
+	if($('.slider-container ul').children().size()>0) {
+		$('.slider-container ul').anoSlide({
+			items: 1,
+			speed: 500,
+			lazy: true,
+			auto: 4000,
+			onStart: function(ui) {
+				var index = ui.index;
+				$('.slider-btns li a').removeClass();
+				$('.slider-btns li:eq( '+index+' ) a').addClass('active-slide');
+			}
+		});
+	}
 });
