f4d31ff37512f17fb5f6ceec66ae495c26ab1a07
Ubuntu <ubuntu@ip-172-31-39-109.us-west-2.compute.internal>
2016-03-27 23:10:43 +0000

    Sync v2

--- sync.sh
+++ sync.sh
@@ -1,2 +1,40 @@
-s3cmd sync public/* -P --verbose --add-header=cache-control:max-age=16070400,public s3://ternopel
-#s3cmd -m "text/css" --add-header=content-type:"text/css" sync public/* -P --verbose --add-header=cache-control:max-age=16070400,public s3://ternopel
+function compress() {
+
+	extension=$1
+	mime=$2
+
+	echo "Removing /tmp/public ..."
+	rm -fR /tmp/public
+	echo "Creating /tmp/public ..."
+	mkdir /tmp/public
+	echo "Copying files $extension to /tmp/public ..."
+	cp --parents `find public -name "$extension"` /tmp
+	echo "Compressing files $extension ..."
+	find /tmp/public -name "$extension" -print -exec gzip {} \; -exec mv {}.gz {} \;
+	echo "Sending files $extension to s3 ..."
+	s3cmd -m $mime --add-header=content-type:$mime sync /tmp/public/* -P --verbose --add-header=cache-control:max-age=16070400,public --add-header=content-encoding:gzip s3://ternopel
+	echo "Done !!"
+
+}
+
+function nocompress() {
+
+	extension=$1
+	mime=$2
+
+	echo "Removing /tmp/public ..."
+	rm -fR /tmp/public
+	echo "Creating /tmp/public ..."
+	mkdir /tmp/public
+	echo "Copying files $extension to /tmp/public ..."
+	cp --parents `find public -name "$extension"` /tmp
+	echo "Sending files $extension to s3 ..."
+	s3cmd -m $mime --add-header=content-type:$mime sync /tmp/public/* -P --verbose --add-header=cache-control:max-age=16070400,public s3://ternopel
+	echo "Done !!"
+
+}
+
+compress "*.css" "text/css"
+compress "*.js" "application/javascript"
+nocompress "*.jpg" "image/jpeg"
+nocompress "*.png" "image/png"
