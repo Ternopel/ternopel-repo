e8bb61f3cd0925f6c13de7f2dd38167f30907c96
Maximiliano <mcarrizo@ternopel.com>
2016-04-10 12:10:10 -0300

    .

--- app/controllers/productspictures_controller.js
+++ app/controllers/productspictures_controller.js
@@ -144,7 +144,7 @@ module.exports = {
 				res.setHeader('Content-Disposition', 'inline; filename='+picture_id);
 //				res.setHeader('Cache-Control', 'max-age=16070400,public');
 //				res.setHeader('Expires', 'Mon, 03 Nov 2050 23:16:20 GMT');
-				res.setHeader('Last-Modified', productpicture.last_update.toUTCString());
+//				res.setHeader('Last-Modified', productpicture.last_update.toUTCString());
 				
 				logger.info("Sending picture to browser");
 				return res.send(data);
