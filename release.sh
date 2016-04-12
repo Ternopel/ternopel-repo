2ed8de0d24efd3405e9c895625fbfbba3013caa4
Maximiliano <mcarrizo@ternopel.com>
2016-04-12 19:06:46 -0300

    .

--- release.sh
+++ release.sh
@@ -1,10 +1,8 @@
-export CLASSPATH="postgresql-9.3-1101-jdbc4.jar;liquibase-core-2.0.3.jar"
+export CLASSPATH="support/liquibase/postgresql-9.3-1101-jdbc4.jar;support/liquibase/liquibase-core-2.0.3.jar"
 java liquibase.integration.commandline.Main \
       --driver=org.postgresql.Driver \
-      --changeLogFile=liquibase.xml \
+      --changeLogFile=support/liquibase/liquibase.xml \
       --url=jdbc:postgresql://localhost:5432/ternopel?charSet=UTF-8 \
       --username=postgres \
       --password=Pilarcita1 \
-      releaseLocks
-
-
+      $1
