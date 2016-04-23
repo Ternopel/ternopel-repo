694c40bd569179d956a6e55c6567e6fd139611bb
Maximiliano <mcarrizo@ternopel.com>
2016-04-23 19:35:59 -0300

    .

--- public/javascript/listdelivery.js
+++ public/javascript/listdelivery.js
@@ -5,7 +5,8 @@ $(function() {
 	form.find('button[name="send"]').click(function() {
 		make_form('listdelivery_form', 
 			function (successresponse) {
-				window.location.href	= '/admin/listdelivery';
+				show_error_messages({responseText:"[{\"param\":\"general\",\"msg\":\"Correo ha sido correctamente enviado !!.\"}]"});
+				$('input[name="email_address"]').val('');
 			},
 			function (errorresponse) {
 				show_error_messages(errorresponse);
